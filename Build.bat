ce4a99a373c60fdeff2d769897b596ff929ed897
bekkolya <62704279+bekkolya@users.noreply.github.com>
2021-09-19 14:26:49 +0300

    Add files via upload

---
+++ Build.bat
@@ -0,0 +1,2 @@
+cd C:\Users\bekk-\source\repos\myproject\MyCalcLib
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
---
+++ Test.bat
@@ -0,0 +1,2 @@
+cd C:\Users\bekk-\source\repos\myproject\MyCalcLib\MyCalcLibTests\bin\Debug
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" MyCalcLibTests.dll
